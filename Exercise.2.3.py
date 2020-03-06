e4cb42cf414e3f8a92a7e6e5032c8cd684824d9d
Apoorva Mishra <apoorvashukla88@gmail.com>
2020-03-05 22:06:01 -0500

    exercise2

--- Exercise.2.3.py
+++ Exercise.2.3.py
@@ -1,1 +1,19 @@
 import random
+
+
+def evaluate_number(guessTaken, number):
+
+    if number < guessTaken :
+       print("Too Low")
+    elif number > guessTaken :
+        print("Too High")
+    elif number == guessTaken :
+       print("Your number is " + str(number))
+    elif number != guessTaken :
+        print("")
+
+
+guessTaken = random.randint(1, 5)
+print("Guessed number is  " + str(guessTaken))
+number = input("Give the number ")
+evaluate_number (int(guessTaken), int(number))
